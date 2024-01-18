a84d89fb146e41610e0273c8c50b785a9b775fe9
Khaled ElGabry <iKhaledElGabry@gmail.com>
2024-01-18 15:13:03 +0200

    Adding ProductDetails in pages

--- React_Ecommerce/src/pages/ProductDetails.js
+++ React_Ecommerce/src/pages/ProductDetails.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { API_KEY } from "../apis";
+import { API_KEYS } from "../apis";
 
 
 export default function ProductDetails() {
-
+     
 }
