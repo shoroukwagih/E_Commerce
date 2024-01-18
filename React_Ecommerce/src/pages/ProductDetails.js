57b7de1f57b302266f79a259d52e8eab58b465f6
Khaled ElGabry <iKhaledElGabry@gmail.com>
2024-01-18 15:20:42 +0200

    Adding some statements at ProductDetails file

--- React_Ecommerce/src/pages/ProductDetails.js
+++ React_Ecommerce/src/pages/ProductDetails.js
@@ -1,8 +1,11 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import { API_KEYS } from "../apis";
+// import React from "react";
+// import { useParams } from "react-router-dom";
+// import { API_KEYS } from "../apis/";
+// import
+// import
 
 
-export default function ProductDetails() {
+// export default function ProductDetails() {
+     // const productDetails;
      
-}
+// }
