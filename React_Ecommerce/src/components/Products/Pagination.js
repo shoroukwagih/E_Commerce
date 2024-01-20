8f0153f5c2e2fcff285c3d455a37a0deeea69b66
Shorouk Wagih <shoroukwagih1511@gmail.com>
2024-01-20 18:04:14 +0200

    last commit

---
+++ React_Ecommerce/src/components/Products/Pagination.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Pagination } from "react-bootstrap";
+
+const PaginatedList = ({ paginationFun }) => {
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+    paginationFun(pageNumber);
+  };
+
+  return (
+    <div>
+      <Pagination>
+        {Array.from({ length: 5 }).map(
+          (_, index) => (
+            <Pagination.Item
+              key={index + 1}
+              active={index + 1 === currentPage}
+              onClick={() => paginate(index + 1)}
+            >
+              {index + 1}
+            </Pagination.Item>
+          )
+        )}
+      </Pagination>
+    </div>
+  );
+};
+
+export default PaginatedList;
--- React_Ecommerce/src/components/Products/ProductList.js
+++ React_Ecommerce/src/components/Products/ProductList.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
-
 import ProductCard from "./ProductCard";
 import { axiosInstance } from "../../apis/config";
+import Paginations from "./Pagination";
 
 const ProductList = () => {
-    const [product, setProducts] = useState([]);
+  const [product, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 6;
 
   useEffect(() => {
     axiosInstance
@@ -12,25 +14,33 @@ const ProductList = () => {
       .then((res) => setProducts(res.data.products))
       .catch((err) => console.log(err));
   }, []);
-  ////////////////////////////////////
-    return (
-        <>
-            <h1>Product List</h1>
-            <hr />
-            <div className="container">
-            <div className="row g-4">
-            {product.map((product) => {
-                    return (
-                        <div className="col-lg-3 col-md-6 col-12 d-flex  justify-content-center align-items-center" key={product.id}>
-                            <ProductCard productItem={product}/>
-                        </div>
-                    );
-                })}
-            </div>
-            </div>
-           
-        </>
-    )
-}
-export default ProductList;
 
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = product.slice(indexOfFirstProduct, indexOfLastProduct);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  return (
+    <>
+      <h1>Product List</h1>
+      <hr />
+      <div className="container">
+        <div className="row g-4">
+          {currentProducts.map((product) => (
+            <div className="col-lg-4 col-md-6 col-12 mb-3" key={product.id}>
+              <ProductCard productItem={product} />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="d-flex align-items-center justify-content-center mt-3">
+        <Paginations paginationFun={(page) => paginate(page)} />
+      </div>
+    </>
+  );
+};
+
+export default ProductList;
